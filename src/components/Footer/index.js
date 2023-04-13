f71b4642eeb9d80485ce094ca8390e35b7e26fde
Crystal Andre <candre02.2@gmail.com>
2023-04-12 19:50:11 -0500

    created index file for the footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+// import files
+import React from "react";
+
+
+// footer function
+function Footer() {
+    return (
+        <footer>
+            <div className="">
+            {/* <p>Created by Crystal Andre <br></br>&copy;2022</p> */}
+                <a href="https://www.github.com/candre02">Github</a>
+                <a href="https://www.linkedin.com/in/crystal-a-6ab60b231/">Linkedin</a>
+                <a href="https://stackoverflow.com/users/16661619/crystal">Stackoverflow</a>
+            </div>
+        </footer>
+    );
+}
+
+// export footer
+export default Footer;
