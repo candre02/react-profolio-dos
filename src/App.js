2df92f85d117d564d3cb8f13f98d2c236fc57e00
Crystal Andre <candre02.2@gmail.com>
2023-04-12 19:13:21 -0500

    created assets,images,componenets,utils folders, added in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import files
+import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+// import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import HomePage from './components/HomePage';
 
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+// app function
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </Router>
   );
 }
 
+// export app
 export default App;
